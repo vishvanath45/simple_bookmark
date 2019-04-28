e6dfe6a7af35db715117499209b36af0532a0fd3
vishvanath45 <visitdutt@gmail.com>
2019-04-28 17:20:51 +0530

    Add Headers in request to fetch title

--- main.py
+++ main.py
@@ -47,15 +47,23 @@ def show_bookmark(word):
     else:
         return "Key does not exists!"
 
+
+
 def return_title(url_):
     import urllib.request as urllib2
     from bs4 import BeautifulSoup
     try:
-        soup = BeautifulSoup(urllib2.urlopen(str(url_)))
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+        req = urllib2.Request(url_,headers=hdr)
+        soup = BeautifulSoup(urllib2.urlopen(req))
         title = soup.title.string
     except:
         title = "Unable to fetch Title"
-    print(title)
     return title
 
 if __name__ == '__main__':
